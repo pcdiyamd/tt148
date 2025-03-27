591d4e86ce48d0b366543cdbfa6749dcc81cd6e8
peter <pcdiyamd@gmail.com>
2025-03-27 20:11:55 +0800

    hide about, Links. edit：title, subtitle, lang. del：md file at post X 2,edit：about.md

--- src/config.ts
+++ src/config.ts
@@ -42,11 +42,11 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Home,
     LinkPreset.Archive,
     LinkPreset.About,
-    /*{
+    /* {
       name: 'GitHub',
       url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
-    },*/
+    }, */
   ],
 }
 
@@ -54,7 +54,7 @@ export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/dm-avatar.webp',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'Lorem Ipsum',
   bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  /*links: [
+  links: [
     {
       name: 'Twitter',
       icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
@@ -72,7 +72,7 @@ export const profileConfig: ProfileConfig = {
       icon: 'fa6-brands:github',
       url: 'https://github.com/saicaca/fuwari',
     },
-  ],*/
+  ],
 }
 
 export const licenseConfig: LicenseConfig = {
--- src/content/spec/about.md
+++ src/content/spec/about.md
@@ -1,9 +1,8 @@
-# About
-This is the demo site for [Fuwari](https://github.com/saicaca/fuwari).
+# 關於
 
-::github{repo="saicaca/fuwari"}
+### 只想著要紀錄，卻又覺得麻煩的人
 
-> ### Sources of images used in this site
-> - [Unsplash](https://unsplash.com/)
-> - [星と少女](https://www.pixiv.net/artworks/108916539) by [Stella](https://www.pixiv.net/users/93273965)
-> - [Rabbit - v1.4 Showcase](https://civitai.com/posts/586908) by [Rabbit_YourMajesty](https://civitai.com/user/Rabbit_YourMajesty)
+以前有試著使用Hexo、Hugo，雖方便，但不滿意就放著。之後在架設Arch Linux時逛到[這裡](https://blog.yuuzi.cc/posts/notes/good-looking-fastfetch-configuration/)才發現Astro，主要是這個主題看著順眼。
+
+- 有在玩的遊戲：FO76 (Steam)
+- 目前家裡架著Arch Linx，使用Adguard home與immich
