0dab5a9baab8f107c4bbb9bba32076890fd4cd9b
peter <pcdiyamd@gmail.com>
2025-03-27 20:58:24 +0800

    del：twitter, github, discord links. edit：avater, bio

--- src/config.ts
+++ src/config.ts
@@ -47,27 +47,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/dm-avatar.webp',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  links: [
-    {
-      name: 'Twitter',
-      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
-                                        // You will need to install the corresponding icon set if it's not already included
-                                        // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
-    },
-    {
-      name: 'Steam',
-      icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
-    },
-    {
-      name: 'GitHub',
-      icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
-    },
-  ],
+  name: '小孩',
+  bio: '體型雖小，但能量異常',
 }
 
 export const licenseConfig: LicenseConfig = {
